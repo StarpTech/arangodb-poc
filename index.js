2f58fc399b9dc41a9ebc3477d36764553bf91651
Dustin Deus <deusdustin@gmail.com>
2016-08-17 21:47:08 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const arangojs = require('arangojs');
+
+let db = arangojs({
+  url: `http://root:root@127.0.0.1:8529`,
+  databaseName: 'test'
+});
+
+let collection = db.collection('partners');
+
+//collection.create();
+
+let doc = {
+  _key: 'foo',
+  name: 'Foo',
+  lastname: 'bar'
+};
+
+collection.save(doc).then(
+  meta => console.log('Document saved:', meta._rev),
+  err => console.error('Failed to save document:', err)
+);
